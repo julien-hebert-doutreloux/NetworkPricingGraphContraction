899b576e2fdec6e863d1a1dcd6eed65570907e7c
54 <julien.hebert-doutreloux@umontreal.ca>
2024-04-22 14:36:22 -0400

    sh scritp

--- src/sh/2-2-original.sh
+++ src/sh/2-2-original.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=5G
-#SBATCH --time=03:00:00
+#SBATCH --time=03:30:00
 #SBATCH --output=/dev/null
 #SBATCH --partition=optimum
 module load julia
 module load gurobi
-julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d40-01-P.json' './data/generated/problems/paper/original/000000-000000-d40-01-R.json' 5000
-julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d40-03-P.json' './data/generated/problems/paper/original/000000-000000-d40-03-R.json' 5000
+julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d50-06-P.json' './data/generated/problems/paper/original/000000-000000-d50-06-R.json' 5000
+julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d50-07-P.json' './data/generated/problems/paper/original/000000-000000-d50-07-R.json' 5000
 sleep 60
--- src/sh/4-2-original.sh
+++ src/sh/4-2-original.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=5G
-#SBATCH --time=03:00:00
+#SBATCH --time=03:30:00
 #SBATCH --output=/dev/null
 #SBATCH --partition=optimum
 module load julia
 module load gurobi
-julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d45-01-P.json' './data/generated/problems/paper/original/000000-000000-d45-01-R.json' 5000
-julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d45-03-P.json' './data/generated/problems/paper/original/000000-000000-d45-03-R.json' 5000
+julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-d50-10-P.json' './data/generated/problems/paper/original/000000-000000-d50-10-R.json' 5000
+julia ./src/julia/script.jl './data/generated/problems/paper/original/000000-000000-g30-01-P.json' './data/generated/problems/paper/original/000000-000000-g30-01-R.json' 5000
 sleep 60
