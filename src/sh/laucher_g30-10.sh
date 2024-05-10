061a8fc8dfaab68a3d6ef73a4198344241515265
fiftyfour <julien.hebert-doutreloux@umontreal.ca>
2024-05-10 12:48:04 -0400

    cleaning

--- src/sh/laucher_g30-10.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=1G
-#SBATCH --time=01:00:00
-#SBATCH --output=/dev/null
-#SBATCH --partition=optimum
-module load julia
-module load gurobi
-sleep 600
